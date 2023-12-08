e4dcb5b31a632f91027fdfec65e2ef228aec1926
spg2143 <spg2143@columbia.edu>
2023-12-08 02:01:49 -0500

    changes to ensemble

--- builder/modules/models/ensemble.py
+++ builder/modules/models/ensemble.py
@@ -33,12 +33,31 @@ class ensemble_wrapper(ForecastingModels):
 
         return merged
 
-    def calculate_ensemble_forecast(self, merged_predictions):
+    def calculate_weights(self, merged_predictions):
         """
-        Calculate the ensemble forecast by averaging the forecast values of all models.
+        Calculate weights for each model inversely proportional to their mean accuracy.
         """
-        forecast_columns = [col for col in merged_predictions.columns if 'forecast' in col]
-        merged_predictions['ensemble_forecast'] = merged_predictions[forecast_columns].mean(axis=1)
+        weight_columns = {}
+        for col in merged_predictions.columns:
+            if 'accuracy' in col:
+                model_name = col.split('_')[0]
+                mean_accuracy = merged_predictions[col].mean()
+                weight_columns[model_name] = 1 / mean_accuracy if mean_accuracy != 0 else 0
+
+        total_weight = sum(weight_columns.values())
+        normalized_weights = {k: v / total_weight for k, v in weight_columns.items()}
+
+        return normalized_weights
+
+    def calculate_weighted_ensemble_forecast(self, merged_predictions, weights):
+        """
+        Calculate the weighted ensemble forecast.
+        """
+        merged_predictions['ensemble_forecast'] = 0
+        for model, weight in weights.items():
+            forecast_col = f'{model}_forecast'
+            merged_predictions['ensemble_forecast'] += weight * merged_predictions[forecast_col]
+
         return merged_predictions
 
     def evaluate_ensemble(self, merged_predictions):
@@ -53,6 +72,7 @@ class ensemble_wrapper(ForecastingModels):
         Run the ensemble model and return the final dataframe with ensemble predictions.
         """
         merged_predictions = self.merge_predictions()
-        merged_predictions = self.calculate_ensemble_forecast(merged_predictions)
+        weights = self.calculate_weights(merged_predictions)
+        merged_predictions = self.calculate_weighted_ensemble_forecast(merged_predictions, weights)
         final_predictions = self.evaluate_ensemble(merged_predictions)
         return final_predictions
