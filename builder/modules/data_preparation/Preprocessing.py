90d21ac817cc789c608ff8bde4e6a67319c0908b
rhonish-nair <95144597+rhonish-nair@users.noreply.github.com>
2023-12-12 20:04:00 -0500

    Add files via upload

--- builder/modules/data_preparation/Preprocessing.py
+++ builder/modules/data_preparation/Preprocessing.py
@@ -9,14 +9,19 @@ train = s.filter_df(new_data, 'schedule_season', 2016, 'leq')
 test = s.filter_df(new_data, 'schedule_season', 2017, 'greq')
 
 def preprocess_data(train, test):
+    '''
+        Split the data accordingly; assigns dummy variables to categorical variables like surface and type
+        train: train dataframe 
+        test: test dataframe
+    '''
     train_exog_surface = pd.get_dummies(train['stadium_surface'], drop_first=True)
     train_exog_type = pd.get_dummies(train['stadium_type'], drop_first=True)
 
     test_exog_surface = pd.get_dummies(test['stadium_surface'], drop_first=True)
     test_exog_type = pd.get_dummies(test['stadium_type'], drop_first=True)
 
-    train_exog = pd.concat([train[['home_away', 'stadium_capacity']], train_exog_surface, train_exog_type], axis=1)
-    test_exog = pd.concat([test[['home_away', 'stadium_capacity']], test_exog_surface, test_exog_type], axis=1)
+    train_exog = pd.concat([train[['home_away', 'stadium_capacity', 'weather_temperature', 'weather_wind_mph', 'home_team_wins', 'away_team_wins']],train_exog_surface, train_exog_type], axis = 1)
+    test_exog = pd.concat([test[['home_away', 'stadium_capacity', 'weather_temperature', 'weather_wind_mph', 'home_team_wins', 'away_team_wins']],test_exog_surface, test_exog_type], axis = 1)
 
     train_exog.index = train['schedule_date']
     test_exog.index = test['schedule_date']
